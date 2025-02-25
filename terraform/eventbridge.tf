3365e212c4f09089817d756fc60289e88dda3b07
Ben Morgan <benmorgan@Bens-MacBook-Pro.local>
2025-02-25 11:51:07 +0000

    adding some adjustments to cloudwatch and eventbridge

--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -1,67 +1,43 @@
-module "eventbridge" {
-  source = "terraform-aws-modules/eventbridge/aws"
-
+module "eventbridge_bus" {
+  source  = "terraform-aws-modules/eventbridge/aws"
   bus_name = "my-bus"
-
-  tags = {
-    Name = "my-bus"
-  }
 }
 
-module "eventbridge" {
-  source = "terraform-aws-modules/eventbridge/aws"
-
-  bus_name = "my-bus"
-
-  create_targets = false
+module "eventbridge_rules" {
+  source  = "terraform-aws-modules/eventbridge/aws"
+  
+  bus_name = module.eventbridge_bus.eventbridge_bus_name
+  create_bus = false  # Bus is created by `eventbridge_bus`
 
   rules = {
     logs = {
       description   = "Capture log data"
       event_pattern = jsonencode({ "source" : ["my.app.logs"] })
     }
-  }
-}
-
-module "eventbridge" {
-  source = "terraform-aws-modules/eventbridge/aws"
-
-  bus_name = "my-bus"
-
-  rules = {
-    logs = {
-      description   = "Capture log data"
-      event_pattern = jsonencode({ "source" : ["my.app.logs"] })
-    }
-  }
-
-  targets = {
-    logs = [
-      {
-        name = "send-logs-to-cloudwatch"
-        arn  = aws_iam_policy_document.cw_document.arn
-      }
-    ]
-  }
-}
-
-module "eventbridge" {
-  source = "terraform-aws-modules/eventbridge/aws"
-
-  create_bus = false
-
-  rules = {
     crons = {
       description         = "Trigger for a Lambda"
       schedule_expression = "rate(15 minutes)"
     }
   }
+}
+
+module "eventbridge_targets" {
+  source  = "terraform-aws-modules/eventbridge/aws"
+  
+  bus_name = module.eventbridge_bus.eventbridge_bus_name
+  create_bus = false  
 
   targets = {
+    logs = [
+      {
+        name = "send-logs-to-cloudwatch"
+        arn  = aws_cloudwatch_log_group.log_group.arn 
+      }
+    ]
     crons = [
       {
         name  = "lambda-loves-cron"
-        arn   = "${aws_iam_role.lambda_1_role.arn}"
+        arn   = aws_lambda_function.my_lambda.arn  
         input = jsonencode({"job": "cron-by-rate"})
       }
     ]
