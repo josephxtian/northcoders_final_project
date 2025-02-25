5dc9977d90a7193c9766024ccd1acfdff9375c3b
Tasneem Aiar <tasneemaiar@Tasneems-MacBook-Pro.local>
2025-02-25 12:26:03 +0000

    connected to db and loaded all data into seperate json files

---
+++ terraform/iam.tf
@@ -0,0 +1,59 @@
+/*
+# ---------------
+# Lambda IAM Role 
+# ---------------
+data "aws_iam_policy_document" "trust_policy" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+resource "aws_iam_role" "lambda_role" {
+  name_prefix        = "role-${var.lambda_name}"
+  assume_role_policy = data.aws_iam_policy_document.trust_policy.json
+}
+# ------------------------------
+# Lambda IAM Policy for S3 Write
+# ------------------------------
+data "aws_iam_policy_document" "s3_data_policy_doc" {
+  statement {
+    actions = ["s3:GetObject"]
+    resources = [
+      "${aws_s3_bucket.data_bucket.arn}/*"
+    ]
+  }
+}
+resource "aws_iam_policy" "s3_write_policy" {
+  name_prefix = "s3-policy-${var.lambda_name}-write"
+  policy      = data.aws_iam_policy_document.cw_document.json
+}
+# Attach
+resource "aws_iam_role_policy_attachment" "lambda_s3_write_policy_attachment" {
+  role = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.s3_write_policy.arn
+}
+*/
+# Lambda
+# https://spacelift.io/blog/terraform-aws-lambda
+# Create IAM role
+# Add IAM policies - check the below policies might be needed
+#   logs:CreateLogGroup
+#   logs:CreateLogStream
+#   logs:PutLogEvents
+# Add IAM policies to the IAM role
+resource "aws_iam_role" "lambda_1_role" {
+  name_prefix        = "role-${var.lambda_1_name}"
+  assume_role_policy = data.aws_iam_policy_document.trust_policy.json # check this
+}
+resource "aws_iam_role" "lambda_2_role" {
+  name_prefix        = "role-${var.lambda_2_name}"
+  assume_role_policy = data.aws_iam_policy_document.trust_policy.json # check this
+}
+resource "aws_iam_role" "lambda_3_role" {
+  name_prefix        = "role-${var.lambda_3_name}"
+  assume_role_policy = data.aws_iam_policy_document.trust_policy.json # check this
+}
---
+++ terraform/lambda.tf
@@ -0,0 +1,61 @@
+/* # copied from previous work
+resource "aws_lambda_function" "s3_file_reader" {
+    function_name = "${var.lambda_name}"
+    s3_bucket = aws_s3_bucket.ingestion_bucket.bucket
+    s3_key = "s3_file_reader/function.zip"
+    role = aws_iam_role.lambda_role.arn
+    handler = "reader.lambda_handler"
+    runtime = "python3.9"
+}
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "${path.module}/../src/file_reader/reader.py"
+  output_path = "${path.module}/../function.zip"
+}
+resource "aws_lambda_permission" "allow_s3" {
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.s3_file_reader.function_name
+  principal = "s3.amazonaws.com"
+  source_arn = aws_s3_bucket.data_bucket.arn
+  source_account = data.aws_caller_identity.current.account_id
+}
+*/
+# lambda function to import from database and put into ingestion s3 bucket
+resource "aws_lambda_function" "lambda_raw_data_to_ingestion_bucket" {
+filename                       = "${data.archive_file.zip_raw_data_to_ingestion_bucket.output_path}"
+function_name                  = "${var.lambda_1_name}-function"
+role                           = aws_iam_role.lambda_1_role.arn
+handler                        = "index.lambda_handler"
+runtime                        = "${var.python_runtime}"
+}
+data "archive_file" "zip_raw_data_to_ingestion_bucket" {
+type        = "zip"
+source_dir  = "${path.module}/${var.lambda_1_name}/"
+output_path = "${path.module}/python_zips/${var.lambda_1_name}.zip"
+}
+# lambda to process information between ingestion_bucket and processed_bucket
+resource "aws_lambda_function" "lambda_ingestion_to_processed_bucket" {
+filename                       = "${data.archive_file.zip_ingestion_to_processed_bucket.output_path}"
+function_name                  = "${var.lambda_2_name}-function"
+role                           = aws_iam_role.lambda_2_role.arn
+handler                        = "index.lambda_handler"
+runtime                        = "${var.python_runtime}"
+}
+data "archive_file" "zip_ingestion_to_processed_bucket" {
+type        = "zip"
+source_dir  = "${path.module}/${var.lambda_2_name}/"
+output_path = "${path.module}/python_zips/${var.lambda_2_name}.zip"
+}
+# lambda to move data from processed_bucket to data warehouse
+resource "aws_lambda_function" "lambda_processed_bucket_to_warehouse" {
+filename                       = "${data.archive_file.zip_processed_bucket_to_warehouse.output_path}"
+function_name                  = "${var.lambda_3_name}-function"
+role                           = aws_iam_role.lambda_3_role.arn
+handler                        = "index.lambda_handler"
+runtime                        = "${var.python_runtime}"
+}
+data "archive_file" "zip_processed_bucket_to_warehouse" {
+type        = "zip"
+source_dir  = "${path.module}/${var.lambda_3_name}/"
+output_path = "${path.module}/python_zips/${var.lambda_3_name}.zip"
+}
