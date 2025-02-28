c4109a682ee98934ce9fd68e43aff84c7465879d
josephxtian <10685917+josephxtian@users.noreply.github.com>
2025-02-28 17:15:12 +0000

    continued development of dimensions table function

--- src/process_ingestion_data_for_dimensions.py
+++ src/process_ingestion_data_for_dimensions.py
@@ -10,17 +10,35 @@ It will remodel it into the following dimension tables:
 
 (Note that data types in some columns may have to be changed to conform to the warehouse data model.)
 """
-# DEPENDENCIES
-# use get_request_function to access files in the s3 bucket
-# pg8000
+
+# FUNCTIONS REQUIRED FOR MAKING - ADD TO TO-DO LIST?
+# get bucket name
+# get 
+
+import pg8000.native
+from s3_read_function.s3_read_function import read_file_from_s3
+from ingestion_to_processed_bucket.dimensions_tables import dimensions_tables_creation
+
+bucket_name = "mock-bucket-name"
 
 # FUNCTION
+
+# table names
+
 # take the data as variable
 # for each table loop:
+
 #   create tables for the dimensions tables using pg8000
 #   sort it into the correct dim tables using pg8000
 # return each dimension table as a variable
 
-# MAYBE OUT OF SCOPE
-# turn back into a file
-# upload it to the processed_bucket
+
+# dimension_table_names = ["dim_date", "dim_staff", "dim_location","dim_currency","dim_design","dim_counterparty"]
+# dimension_tables = [read_file_from_s3(bucket_name, dim_tab_name) for dim_tab_name in dimension_table_names]
+
+
+#"dim_date"
+# "dim_staff", "dim_location","dim_currency","dim_design","dim_counterparty"
+dim_currency = 
+
+INSERT INTO dim_currency ()
