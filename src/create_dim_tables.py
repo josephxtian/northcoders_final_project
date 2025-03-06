fc5cff53e1f21318e9fc339204ce746d85a0d361
josephxtian <10685917+josephxtian@users.noreply.github.com>
2025-03-06 12:28:54 +0000

    fix bugs and error handling in put_info_into_dims_schema, and make all tests work

--- src/create_dim_tables.py
+++ src/create_dim_tables.py
@@ -2,94 +2,55 @@ import pg8000.native
 from s3_read_function.s3_read_function import read_file_from_s3
 
 # This function will set up the dimensions tables
-def set_up_dims_table(database_connection):
-    for table in dimensions_tables_creation:
-        database_connection.run(f'''
-        DROP TABLE IF EXISTS {table};
-        {dimensions_tables_creation[table]};
-        ''')
+def set_up_dims_table(database_connection,table_names):
+    # delete all existing dim tables
+    for dim_table in dimensions_tables_creation:
+        database_connection.run(f"DROP TABLE IF EXISTS {dim_table};")
+    # create list of tables to be created from 
+    dim_tables_created = []
+    # check dependent tables exist for each dim table
+    for dim_table in dimensions_tables_creation:
+      if all(dependent in table_names for dependent in list(dimensions_tables_creation[dim_table][1:])):
+        dim_tables_created.append(dim_table)
+    if not dim_tables_created:
+      raise Exception("Not enough data to create any dim tables, please ensure adequate data has been provided")
+    # create dim tables that can be created
+    for table in dim_tables_created:
+        database_connection.run(f"CREATE TEMPORARY TABLE {dimensions_tables_creation[table][0]};")
+    # return the names of dim tables created
+    return dim_tables_created
 
-
-def put_information_into_dims_schema(database_connection,*input_data):
-
+def put_info_into_dims_schema(database_connection,dim_tables_created):
+    dimension_value_rows = {}
     # use select statement to choose information required for star schema
-    for table in dimensions_tables_creation:
-        database_connection.run(f'''
-        INSERT INTO {table} ({dim_tables_column_headers[table]})
-        VALUES (
-        SELECT
-        created_at,
-        last_updated,
-        agreed_delivery_date,
-        agreed_payment_date
-        FROM purchase_order
-        )
+    for table in dim_tables_created:
+        if table == "dim_date":
+          #here
+            continue
+        elif table == "dim_currency":
+            continue
+        elif table not in ["dim_staff","dim_location","dim_design","dim_counterparty"]:
+           raise Exception("Dimension table names requested are not valid")
+        dimension_value_rows[table] =  database_connection.run(f'''
+        INSERT INTO {table}
+        {dimensions_insertion_queries[table]}
+        RETURNING *;
         ''')
+    # raise error if dimension_value_rows remains empty
+    if dimension_value_rows == {}:
+       raise Exception("No rows outputted")
     # return as variable
-
-        '''INSERT INTO dim_date (date_id,year,month,day,day_of_week,day_name,month_name,quarter)
-        VALUES (
-        SELECT
-        created_at,
-        last_updated,
-        agreed_delivery_date,
-        agreed_payment_date
-        FROM purchase_order
-        )
-        '''
+    return dimension_value_rows
 
 
-
-
-#     # take the data as variable
-#     # for each table loop:
-#     for dim_table in dimensions_tables_creation:
-#     # delete previous tables
-#     # create tables for the dimensions tables
-#         dims_db.run(f'''
-#                 DROP TABLE IF EXISTS {dim_table};   
-#                 {dim_tables_column_headers[dim_table]};
-#                 ''')
-        
-#         # for item in 
-#         dims_db.execute(f'''''')
-                
-
-#         # for dim_staff table
-#         dims_db.execute(f'''
-#         INSERT INTO dim_staff (
-#             staff_id,
-#             first_name,
-#             last_name,
-#             department_name,
-#             location,
-#             email_address
-#         )
-#         VALUES
-#         (SELECT staff_id, first_name, last_name, department_name, location, email_address)
-#         ''')
-
-
-# #   sort it into the correct dim tables using pg8000
-# # return each dimension table as a variable
-
-
-# # dimension_table_names = ["dim_date", "dim_staff", "dim_location","dim_currency","dim_design","dim_counterparty"]
-# # dimension_tables = [read_file_from_s3(bucket_name, dim_tab_name) for dim_tab_name in dimension_table_names]
-
-
-# #"dim_date"
-# # "dim_staff", "dim_location","dim_currency","dim_design","dim_counterparty"
-
-
-# This is a dictionary of dimensions tables 
+# This is a dictionary of lists
+# key = dimensions table names
+# value[0] = headers
+# value[1:] = dependencies
 # for use in python, but written in SQL.
-# It uses block strings to hold the key-value pairs.
-# Below it are lists of column headers
-
 dimensions_tables_creation = {
-  "dim_date":
-'''CREATE TABLE "dim_date" (
+  "dim_date":[
+'''"dim_date" (
   "date_id" date PRIMARY KEY NOT NULL,
   "year" int NOT NULL,
   "month" int NOT NULL,
@@ -98,20 +59,20 @@ dimensions_tables_creation = {
   "day_name" varchar NOT NULL,
   "month_name" varchar NOT NULL,
   "quarter" int NOT NULL
-);''',
+);''',"sales_order"],
 
-"dim_staff":
-'''CREATE TABLE "dim_staff" (
+"dim_staff":[
+'''"dim_staff" (
   "staff_id" int PRIMARY KEY NOT NULL,
   "first_name" varchar NOT NULL,
   "last_name" varchar NOT NULL,
   "department_name" varchar NOT NULL,
   "location" varchar NOT NULL,
   "email_address" varchar NOT NULL
-);''',
+);''',"staff","department"],
 
-"dim_location":
-'''CREATE TABLE "dim_location" (
+"dim_location":[
+'''"dim_location" (
   "location_id" int PRIMARY KEY NOT NULL,
   "address_line_1" varchar NOT NULL,
   "address_line_2" varchar,
@@ -120,25 +81,25 @@ dimensions_tables_creation = {
   "postal_code" varchar NOT NULL,
   "country" varchar NOT NULL,
   "phone" varchar NOT NULL
-);''',
+);''',"address"],
 
-"dim_currency":
-'''CREATE TABLE "dim_currency" (
+"dim_currency":[
+'''"dim_currency" (
   "currency_id" int PRIMARY KEY NOT NULL,
   "currency_code" varchar NOT NULL,
   "currency_name" varchar NOT NULL
-);''',
+);''',"currency"],
 
-"dim_design":
-'''CREATE TABLE "dim_design" (
+"dim_design":[
+'''"dim_design" (
   "design_id" int PRIMARY KEY NOT NULL,
   "design_name" varchar NOT NULL,
   "file_location" varchar NOT NULL,
   "file_name" varchar NOT NULL
-);''',
+);''',"design"],
 
-"dim_counterparty":
-'''CREATE TABLE "dim_counterparty" (
+"dim_counterparty":[
+'''"dim_counterparty" (
   "counterparty_id" int PRIMARY KEY NOT NULL,
   "counterparty_legal_name" varchar NOT NULL,
   "counterparty_legal_address_line_1" varchar NOT NULL,
@@ -148,13 +109,12 @@ dimensions_tables_creation = {
   "counterparty_legal_postal_code" varchar NOT NULL,
   "counterparty_legal_country" varchar NOT NULL,
   "counterparty_legal_phone_number" varchar NOT NULL
-);'''
+);''',"counterparty","address"]
 }
 
 
-dim_tables_column_headers = [
-  ["dim_date",
-  "date_id",
+dim_tables_column_headers = {"dim_date":
+  ["date_id",
   "year",
   "month",
   "day",
@@ -163,16 +123,16 @@ dim_tables_column_headers = [
   "month_name",
   "quarter"],
 
-  ["dim_staff",
-  "staff_id",
+  "dim_staff":
+  ["staff_id",
   "first_name",
   "last_name" ,
   "department_name",
   "location",
   "email_address"],
 
-  ["dim_location",
-  "location_id",
+  "dim_location":
+  ["location_id",
   "address_line_1",
   "address_line_2",
   "district",
@@ -181,20 +141,20 @@ dim_tables_column_headers = [
   "country",
   "phone"],
 
-  ["dim_currency",
-  "currency_id",
+  "dim_currency":
+  ["currency_id",
   "currency_code",
   "currency_name"],
 
 
-  ["dim_design",
-  "design_id",
+  "dim_design":
+  ["design_id",
   "design_name",
   "file_location",
   "file_name"],
 
-  ["dim_counterparty",
-  "counterparty_id",
+  "dim_counterparty":
+  ["counterparty_id",
   "counterparty_legal_name",
   "counterparty_legal_address_line_1",
   "counterparty_legal_address_line_2",
@@ -203,4 +163,44 @@ dim_tables_column_headers = [
   "counterparty_legal_postal_code",
   "counterparty_legal_country",
   "counterparty_legal_phone_number"]
-]
+}
+
+dimensions_insertion_queries = {
+"dim_date":'''
+SELECT created_at,last_updated,agreed_delivery_date,agreed_payment_date
+FROM sales_order
+''',
+"dim_staff":'''
+SELECT staff_id,first_name,last_name, department.department_name,department.location,email_address
+FROM staff
+JOIN department ON staff.department_id = department.department_id
+''',
+"dim_location":'''
+SELECT address_id,address_line_1, address_line_2,district,city,postal_code,country,phone
+FROM address
+''',
+# need to itegrate Erins currency function here
+"dim_currency":'''
+SELECT  currency_id, currency_code
+FROM currency
+''',
+"dim_design":'''
+SELECT design_id,design_name,file_location,file_name
+FROM design
+''',
+"dim_counterparty":'''
+SELECT 
+counterparty_id,
+counterparty_legal_name,
+address.address_line_1,
+address.address_line_2,
+address.district,
+address.city,
+address.postal_code,
+address.country,
+address.phone
+FROM counterparty
+JOIN address
+ON counterparty.legal_address_id = address.address_id
+'''
+}
