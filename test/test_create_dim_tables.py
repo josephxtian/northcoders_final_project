86475c6c437d4eed4443ffa53a69b7e9df8b79d7
josephxtian <10685917+josephxtian@users.noreply.github.com>
2025-03-06 09:09:58 +0000

    updates set_up_dims_table and put_information_into_dims_schema functions

--- test/test_create_dim_tables.py
+++ test/test_create_dim_tables.py
@@ -1,6 +1,7 @@
-from src.create_dim_tables import set_up_dims_table
+from src.create_dim_tables import set_up_dims_table,put_information_into_dims_schema
 import pg8000.native
 from src.connection import connect_to_db, close_db_connection
+from src.create_temporary_tables import make_temporary_tables
 
 # Table headers defined at bottom of document
 
@@ -8,16 +9,18 @@ class TestSetUpDimsTable:
     # test 6 tables have been created
     def test_six_tables_created(self):
         test_db = connect_to_db()
-        set_up_dims_table(test_db)
+        test_input = ["address","counterparty","currency","department","design","sales_order","staff"]
+        set_up_dims_table(test_db,test_input)
         for table in dim_tables_column_headers:
-            test_db.run(f"SELECT * FROM {table[0]};")
+            test_db.run(f"SELECT * FROM {dim_tables_column_headers[table][0]};")
             column_headers = [c['name'] for c in test_db.columns]
-            assert column_headers == table[1:]
+            assert column_headers == dim_tables_column_headers[table]
         close_db_connection(test_db)
 
     def test_tables_are_empty(self):
         test_db = connect_to_db()
-        set_up_dims_table(test_db)
+        test_input = ["address","counterparty","currency","department","design","sales_order","staff"]
+        set_up_dims_table(test_db,test_input)
         for table in dim_tables_column_headers:
             result = test_db.run(f"SELECT * FROM {table[0]};")
             assert result == []
@@ -25,25 +28,38 @@ class TestSetUpDimsTable:
 
 
 class TestPutInformationIntoDimsSchema():
-    # test there are no duplicates
-    def test_no_duplicates_in_data(self):
+    # test 
+    def test_insertion_for_one_table(self):
+        test_input = {"staff":[
+        {"staff_id": 1,
+         "first_name": "Jeremie",
+         "last_name": "Franey",
+         "department_id": 2,
+         "email_address": "jeremie.franey@terrifictotes.com",
+         "created_at": "2022-11-03T14:20:51.563000",
+         "last_updated": "2022-11-03T14:20:51.563000"}],
+         "department": [
+        {"department_id": 1,
+         "department_name": "Sales",
+         "location": "Manchester",
+         "manager": "Richard Roma",
+         "created_at": "2022-11-03T14:20:49.962000",
+         "last_updated": "2022-11-03T14:20:49.962000"
+        }]}
         test_db = connect_to_db()
-        set_up_dims_table(test_db)
-        for table in dim_tables_column_headers:
-            test_db.run(f"SELECT * FROM {table[0]};")
-            column_headers = [c['name'] for c in test_db.columns]
-            assert column_headers == table[1:]
+        func_result = make_temporary_tables(test_db,test_input)
+        table_names = func_result[0]
+        set_up_dims_table(test_db,table_names)
+        result = put_information_into_dims_schema(test_db)
+        print(result)
+        assert result == 0
         close_db_connection(test_db)
 
-    # test ID numbers are stepping
-    def test_id_numbers_stepping(self):
-        pass
 
 
 # Table headers
-dim_tables_column_headers = [
-  ["dim_date",
-  "date_id",
+dim_tables_column_headers = {"dim_date":
+  ["date_id",
   "year",
   "month",
   "day",
@@ -52,16 +68,16 @@ dim_tables_column_headers = [
   "month_name",
   "quarter"],
 
-  ["dim_staff",
-  "staff_id",
+  "dim_staff":
+  ["staff_id",
   "first_name",
   "last_name" ,
   "department_name",
   "location",
   "email_address"],
 
-  ["dim_location",
-  "location_id",
+  "dim_location":
+  ["location_id",
   "address_line_1",
   "address_line_2",
   "district",
@@ -70,20 +86,20 @@ dim_tables_column_headers = [
   "country",
   "phone"],
 
-  ["dim_currency",
-  "currency_id",
+  "dim_currency":
+  ["currency_id",
   "currency_code",
   "currency_name"],
 
 
-  ["dim_design",
-  "design_id",
+  "dim_design":
+  ["design_id",
   "design_name",
   "file_location",
   "file_name"],
 
-  ["dim_counterparty",
-  "counterparty_id",
+  "dim_counterparty":
+  ["counterparty_id",
   "counterparty_legal_name",
   "counterparty_legal_address_line_1",
   "counterparty_legal_address_line_2",
@@ -92,4 +108,4 @@ dim_tables_column_headers = [
   "counterparty_legal_postal_code",
   "counterparty_legal_country",
   "counterparty_legal_phone_number"]
-]
+}
