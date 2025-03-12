0118d4b3d5b7fd3e9a7236ab85def8c86fd7bf6a
josephxtian <10685917+josephxtian@users.noreply.github.com>
2025-03-12 18:04:03 +0000

    dimensions tables made in pandas

--- src/dimensions_data_manipulation.py
+++ src/dimensions_data_manipulation.py
@@ -10,18 +10,59 @@ from src.dimensions_dependents_set_up import load_local_files_for_testing,create
 # Populates dim_tables
 # Takes input of raw ingestion bucket
 def populate_dim_dfs(input_info_df,df_dim_dictionary):
-    print(input_info_df,"<<<<<input_info_df")
-    for table in df_dim_dictionary:
-        print("TABLE>>>>>",table)
-        if table == 'dim_date':
+    # setup output dictionary
+    filled_dim_tables = {}
+    # dependent tables each dimension table relies on
+    dimensions_dependencies = {
+    "dim_date":["sales_order"],
+    "dim_staff":["staff","department"],
+    "dim_location":["address"],
+    "dim_currency":["currency"],
+    "dim_design":["design"],
+    "dim_counterparty":["counterparty","address"]
+    }
+    # iterate through the keys of the dictionary
+    for table_name, dataframe in df_dim_dictionary.items():
+        print("TABLE>>>>>",table_name)
+        if table_name == 'dim_date':
             dates = input_info_df["sales_order"][['created_at','last_updated','agreed_delivery_date','agreed_payment_date']]
+            # print(dates)
             for date in dates:
-                print("DATE>>>>",dates[date])
+                # print("DATE>>>>",dates[date])
                 for indv_date in dates[date]:
                     formatted_date = extract_date_info_from_dim_date(indv_date)
+        elif table_name == "dim_currency":
+            continue
+            # run currency operations
+        # process all the other dim tables
+        elif table_name in ["dim_staff", "dim_location", "dim_design", "dim_counterparty"]:
+            # iterate through the input data staff information
+            dim_table = df_dim_dictionary[table_name]
+            no_of_dependencies = len(dimensions_dependencies[table_name])
+            first_dependency = dimensions_dependencies[table_name][0]
+            raw_info_df = input_info_df[first_dependency]
+            if no_of_dependencies>1:
+                for i in range(1,no_of_dependencies):
+                    print(i,"<<<<<eye")
+                    merging_dependency = dimensions_dependencies[table_name][i]
+                    # define special case for counterparty joining key
+                    if first_dependency == "counterparty":
+                        left_join_key = "legal_address_id"
+                    else:
+                        left_join_key = input_info_df[merging_dependency].index.name
+                    merged_df = raw_info_df.merge(input_info_df[merging_dependency], how="left",left_on=left_join_key,right_index=True)
+                    dim_table = pd.concat([dim_table,merged_df], ignore_index=False,join='inner')
+            else:
+                dim_table = pd.concat([dim_table,raw_info_df], ignore_index=False,join='inner')
+            print(dim_table,f"<<<<<DIMTABLE {table_name}")
 
+            filled_dim_tables[table_name] = dim_table
+            # print(filled_dim_tables,"<<<<<<FILLED_DIM_TABLES")
+        else:
+            # raise exception if dimension table name not valid
+            raise Exception("Dimension table names requested are not valid")
 
-        # df.drop_duplicates(inplace = True)
+    return filled_dim_tables
 
 if __name__ == "__main__":
     import_files = load_local_files_for_testing()
--- src/write_schema_to_processed.py
+++ src/write_schema_to_processed.py
@@ -4,10 +4,12 @@ import io
 import os
 from datetime import datetime
 from utils.get_bucket import get_bucket_name
+from src.dimensions_data_manipulation import populate_dim_dfs
+from src.dimensions_dependents_set_up import load_local_files_for_testing, create_pandas_raw_tables, create_pandas_empty_dim_tables
 
-bucket_name = get_bucket_name("processed-bucket")
+# bucket_name = get_bucket_name("processed-bucket")
 
-S3_BUCKET = os.getenv("S3_BUCKET", bucket_name)
+S3_BUCKET = os.getenv("S3_BUCKET", "processed-bucket20250312152305747100000002")
 
 """
 
@@ -23,7 +25,7 @@ def write_schema_to_processed(data: pd.DataFrame):
 
     s3_client = boto3.client("s3")
 
-    bucket_name = os.getenv(S3_BUCKET)
+    bucket_name = os.getenv("S3_BUCKET", S3_BUCKET)
 
     if not bucket_name:
         raise ValueError(f"{S3_BUCKET} environment variable is not set")
@@ -33,7 +35,7 @@ def write_schema_to_processed(data: pd.DataFrame):
     timestamp = datetime.utcnow().strftime("%Y-%m-%d_%H-%M-%S")
     
 # Set a variable key which dynamically changes the name of each of the pieces of processed data based on one of their unique elements
-    key = f"fact_sales_orders/{source_file.replace('.json', '')}_{timestamp}.parquet"
+    key = f"dim_staff/{source_file.replace('.json', '')}_{timestamp}.parquet"
 
     try:
         # Convert the files into parquet format
@@ -55,3 +57,10 @@ def write_schema_to_processed(data: pd.DataFrame):
         raise
 # Iterate over the list of dictionaries and dump each individual element into a json object
 
+if __name__ == "__main__":
+    import_files = load_local_files_for_testing()
+    info_df_dict = create_pandas_raw_tables(import_files)
+    empty_dim_tables = create_pandas_empty_dim_tables()
+    dim_staff = populate_dim_dfs(info_df_dict,empty_dim_tables)
+    print(dim_staff)
+    write_schema_to_processed(dim_staff)
