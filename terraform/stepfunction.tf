ff8d4104f58de03247f9501d50e86599163c742d
Erin Monaghan <erinjude85@gmail.com>
2025-03-12 12:27:04 +0000

    start of lambda handler 2

--- terraform/stepfunction.tf
+++ terraform/stepfunction.tf
@@ -1,13 +1,45 @@
-# joe to do
 
 resource "aws_sfn_state_machine" "lambda_1_2_3" {
   name     = "lambda-2-state_machine"
-  role_arn = aws_iam_role.step_function_execution_role.arn
+  role_arn = aws_iam_role.step_function_role.arn
+  definition = jsonencode({
+    "Comment": "State Machine to Process Data from Ingestion to Process Bucket",
+    "StartAt": "ReadFromS3",
+    "States": {
+      "ReadFromS3": {
+        "Type": "Task",
+        "Resource": "arn:aws:states:::lambda:invoke",
+        "Parameters": {
+            "FunctionName": "${aws_lambda_function.lambda_ingestion_to_processed_bucket.arn}"
+        },
+        "ProcessData": {
+        "Type": "Task",
+        "Resource": "arn:aws:states:::lambda:invoke",
+        "Parameters": {
+            "FunctionName": "${aws_lambda_function.lambda_ingestion_to_processed_bucket.arn}"
+        },
 
-  definition = templatefile("${path.module}/pipeline.json",
-    { resource          = aws_lambda_function.lambda_ingestion_to_processed_bucket.arn
-      aws_region        = data.aws_region.current.name,
-      aws_account_num   = data.aws_caller_identity.current.account_id,
-      function_name     = var.step_function_name
-    })
+        "End": true
+    }
+})
+
+  depends_on = [aws_lambda_function.lambda_ingestion_to_processed_bucket]
 }
+
+resource "aws_iam_role" "step_function_role" {
+  name = "StepFunctionExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "states.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
