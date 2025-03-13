607adda6ca76acc2b234b76bdfa0fe51b8bde674
Mark Bradley <mark_br@hotmail.co.uk>
2025-03-13 15:06:43 +0000

    lambda layer for warehouse

--- terraform/lambda-layer.tf
+++ terraform/lambda-layer.tf
@@ -19,5 +19,22 @@ resource "aws_lambda_layer_version_permission" "lambda_layer_permission" {
   statement_id   = "test-pg8000-layer"
 }
 
+data "archive_file" "lambda-layer_warehouse" {
+  type = "zip"
+  source_dir = "${path.module}/../py_packages/python/"
+  output_path = "${path.module}/../requirements-package.zip"
+}
 
 
+resource "aws_lambda_layer_version" "layer_write_to_warehouse" {
+  layer_name = "lambda_layer_to_warehouse"
+  filename = "${path.module}/../py_packages/requirements-package.zip"
+}
+
+resource "aws_lambda_layer_version_permission" "lambda_layer_write_to_warehouse_permission" {
+  layer_name     = aws_lambda_layer_version.layer_write_to_warehouse.layer_name
+  version_number = aws_lambda_layer_version.layer_write_to_warehouse.version
+  principal      = "*"
+  action         = "lambda:GetLayerVersion"
+  statement_id   = "test-warehouse-layer"
+}
